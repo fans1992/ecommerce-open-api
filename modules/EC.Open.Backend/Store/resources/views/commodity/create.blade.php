2f15f950df03aa8ae2d7b2994fbfd507e814975a
fanzhuo <fans1992@qq.com>
2019-10-26 08:57:05 +0000

    商品编辑服务亮点

--- modules/EC.Open.Backend/Store/resources/views/commodity/create.blade.php
+++ modules/EC.Open.Backend/Store/resources/views/commodity/create.blade.php
@@ -221,6 +221,14 @@
                     </div>
                 </div>
 
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">服务亮点：</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="service_highlights" placeholder="">
+                    </div>
+                </div>
+
                 <div class="form-group">
                     <label class="col-sm-2 control-label">产品标签：</label>
                     <div class="col-sm-10">
