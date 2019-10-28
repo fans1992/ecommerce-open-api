5131b900114a2b392256e00bb44a4fe28c972b9e
fanzhuo <fans1992@qq.com>
2019-10-28 11:19:24 +0000

    cors fix

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,11 @@ return [
     */
    
     'supportsCredentials' => true,
-    'allowedOrigins' => ['http://shop.foridom.com', 'http://localhost:3030'],
+    'allowedOrigins' => [
+        'http://shop.foridom.com',
+        'http://localhost:3030',
+        'http://127.0.0.1:53892',
+    ],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
     'exposedHeaders' => [],
