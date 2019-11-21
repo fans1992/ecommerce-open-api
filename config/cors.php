9d275253ff1c912bacb138d1f25ecd18f91ccf90
admin <623433882@qq.com>
2019-11-22 00:53:31 +0800

    test

--- config/cors.php
+++ config/cors.php
@@ -16,7 +16,8 @@ return [
     'allowedOrigins' => [
         'http://shop.foridom.com', //测试环境
         'http://localhost:3030', //本地开发
-        'http://127.0.0.1:58795' //小程序
+        'http://127.0.0.1:58795', //小程序
+        'http://118.25.3.152:8090',
     ],
 //    'allowedOrigins' => ['*'],
     'allowedHeaders' => ['*'],
