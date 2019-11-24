48a9b9a0e3226dbcf00c3c0ea33a1426d8c485c6
fanzhuo <henbuhai@hotmail.com>
2019-11-24 16:30:35 +0000

    添加付款提醒

--- modules/EC.Open.Server/src/Schedule/OrderRemindSchedule.php
+++ modules/EC.Open.Server/src/Schedule/OrderRemindSchedule.php
@@ -16,9 +16,15 @@ class OrderRemindSchedule extends Scheduling
 				foreach ($orders as $order) {
 					$order_created_at = strtotime($order->created_at);
 					$order_close_time = app('system_setting')->getSetting('order_auto_cancel_time');
-					if ($order_close_time > 15 && (time() - $order_created_at) >= ($order_close_time - 15) * 60) {
-						$order->update(['is_remind' => 1]);
-					}
+                    if ($order_close_time > 15 && (time() - $order_created_at) >= ($order_close_time - 15) * 60) {
+                        $user = User::find($order->user_id);
+                        $app         = '';
+                        $data        = [];
+                        $openid      = '';
+                        $template_id = 'taSuCz3AVFCp9gZc-_vERJPl2UlCcmNufGvJ3QzFymc';
+                        $user->notify(new OfficialAccountTemplateMessage($data, $openid, $app));
+                        $order->update(['is_remind' => 1]);
+                    }
 				}
 			}
 		})->everyMinute();
