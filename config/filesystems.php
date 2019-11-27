7ddb1d6e9e67e4a49e77d3de6a8eb51f9423dd4e
fanzhuo <fans1992@qq.com>
2019-11-27 20:09:54 +0800

    add aliyun oss

--- config/filesystems.php
+++ config/filesystems.php
@@ -69,6 +69,14 @@ return [
             'bucket'     => env('QINIU_BUCKET', 'test'),
             'domain'     => env('QINIU_DOMAIN', 'xxx.clouddn.com'), // or host: https://xxxx.clouddn.com
         ],
+        'oss' => [
+            'driver' => 'oss',
+            'access_key' => env('OSS_ACCESS_KEY'),
+            'secret_key' => env('OSS_SECRET_KEY'),
+            'endpoint'   => env('OSS_ENDPOINT'),
+            'bucket'     => env('OSS_BUCKET'),
+            'isCName'    => env('OSS_IS_CNAME', false), // 如果 isCname 为 false，endpoint 应配置 oss 提供的域名如：`oss-cn-beijing.aliyuncs.com`，否则为自定义域名，，cname 或 cdn 请自行到阿里 oss 后台配置并绑定 bucket
+        ],
     ],
 
 ];
