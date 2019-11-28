c5b5899b6a39295fbeb4d3339ca382ee5f4aad02
fanzhuo <henbuhai@hotmail.com>
2019-11-28 17:46:54 +0000

    add helper

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Storage;
+
 function ngrok_url($routeName, $parameters = [])
 {
     // 开发环境，并且配置了 NGROK_URL
@@ -11,4 +13,30 @@ function ngrok_url($routeName, $parameters = [])
     }
 
     return route($routeName, $parameters);
+}
+
+
+/**
+ * @param $path
+ * @param $file
+ * @param string $drive
+ * @return bool
+ */
+function upload_image($path, $file, $drive = 'oss')
+{
+    if (!$path) return false;
+
+    //将图片上传到OSS中，并返回图片路径信息 值如:avatar/WsH9mBklpAQUBQB4mL.jpeg
+    $disk = Storage::disk($drive);
+    $path = $disk->put($path, $file);
+
+
+    //由于图片不在本地，所以我们应该获取图片的完整路径，
+    //值如：https://test.oss-cn-hongkong.aliyuncs.com/avatar/8GdIcz1NaCZ.jpeg
+    switch ($drive) {
+        case 'qiniu':
+            return $disk->getUrl($path);
+        case 'oss':
+            return $disk->url($path);
+    }
 }
