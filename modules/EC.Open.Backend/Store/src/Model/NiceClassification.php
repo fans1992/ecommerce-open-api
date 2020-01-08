31b7c6cd1b9753be0d6c3b43f339d7e5cb57bdd3
fanzhuo <fans1992@qq.com>
2020-01-08 18:01:20 +0800

    后台保存分类初始化

--- modules/EC.Open.Backend/Store/src/Model/NiceClassification.php
+++ modules/EC.Open.Backend/Store/src/Model/NiceClassification.php
@@ -14,6 +14,26 @@ class NiceClassification extends Model implements Transformable
 
     protected $guarded = ['id'];
 
+    protected static function boot()
+    {
+        parent::boot();
+        // 监听 NiceClassification 的创建事件，用于初始化 path 和 level 字段值
+        static::creating(function (NiceClassification $niceClassification) {
+            // 如果创建的是一个根类目
+            if (!$niceClassification->parent_id) {
+                // 将层级设为 1
+                $niceClassification->level = 1;
+                // 将 path 设为 /
+                $niceClassification->path  = '/';
+            } else {
+                // 将层级设为父类目的层级 + 1
+                $niceClassification->level = $niceClassification->parent->level + 1;
+                // 将 path 值设为父类目的 path 追加父类目 ID 以及最后跟上一个 - 分隔符
+                $niceClassification->path  = $niceClassification->parent->path.$niceClassification->parent_id.'/';
+            }
+        });
+    }
+
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
