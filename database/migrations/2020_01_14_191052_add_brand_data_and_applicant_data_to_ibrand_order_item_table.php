7f969b34944ebaf0f6155b0908f45cadf94cbeb4
fanzhuo <henbuhai@hotmail.com>
2020-01-14 17:22:18 +0000

    fix

--- database/migrations/2020_01_14_191052_add_brand_data_and_applicant_data_to_ibrand_order_item_table.php
+++ database/migrations/2020_01_14_191052_add_brand_data_and_applicant_data_to_ibrand_order_item_table.php
@@ -14,8 +14,8 @@ class AddBrandDataAndApplicantDataToIbrandOrderItemTable extends Migration
     public function up()
     {
         Schema::table('ibrand_order_item', function (Blueprint $table) {
-            $table->string('brand_data')->after('is_send')->nullable()->comment('商标信息');
-            $table->string('applicant_data')->after('brand_data')->nullable()->comment('申请信息');
+            $table->text('brand_data')->after('is_send')->nullable()->comment('商标信息');
+            $table->text('applicant_data')->after('brand_data')->nullable()->comment('申请信息');
         });
     }
 
